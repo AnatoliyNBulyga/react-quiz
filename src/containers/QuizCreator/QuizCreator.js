a9cf43c804779e53d8eb63c775e44ca8f86c13d1
AnatoliyNBulyga <anatoliynbulyga@gmail.com>
2020-03-23 21:21:46 +0200

    70 add new pages

---
+++ src/containers/QuizCreator/QuizCreator.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+import c from "./QuizCreator.module.css";
+
+export default class QuizeCreator extends Component {
+    render() {
+        return (
+            <div>
+               <h1>QuizeCreator</h1>
+            </div>
+        )
+    }
+}
