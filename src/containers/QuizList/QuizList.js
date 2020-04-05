06c35dbcb679f60d93b5df6023032b346005720b
AnatoliyNBulyga <anatoliynbulyga@gmail.com>
2020-04-05 13:46:17 +0300

    87 loading

--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -1,29 +1,66 @@
 import React, {Component} from "react";
 import c from "./QuizList.module.css";
 import {NavLink} from "react-router-dom";
+import axios from "axios";
+import Loader from "../../components/UI/Loader/Loader";
 
 export default class QuizList extends Component {
+  state = {
+      quizes: [],
+      loading: true
+  }
   renderQuizes() {
-      return [1,2,3].map((quiz, index) => {
+      return this.state.quizes.map((quiz) => {
         return(
-            <li key={index}>
-                <NavLink to={'/quiz/' + quiz}>
-                  Тест {quiz}
+            <li key={quiz.id}>
+                <NavLink to={'/quiz/' + quiz.id}>
+                  {quiz.name}
                 </NavLink>
             </li>
         )
       })
   }
-  render() {
+
+  async componentDidMount() {
+      try {
+          const response  = await axios.get('https://react-quiz-bdd19.firebaseio.com/quizes.json');
+
+          const quizes = [];
+
+          Object.keys(response.data).forEach( (key,index) => {
+              quizes.push({
+                  id: key,
+                  name: `Тест №${index + 1}`
+              })
+          });
+
+          this.setState({
+              quizes,
+              loading: false
+          })
+      } catch (e) {
+          console.log(e)
+      }
+
+  }
+
+    render() {
+
       return (
           <div className={c.quizList}>
               <div>
                   <h1>Список тестов</h1>
-                  <ul>
-                      {this.renderQuizes()}
-                  </ul>
+                  {
+                      (this.state.loading)
+                      ? <Loader/>
+                      : <ul>
+                              {this.renderQuizes()}
+                        </ul>
+                  }
+
               </div>
           </div>
       )
+      
   }
 }
